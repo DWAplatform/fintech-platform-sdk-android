f66798213a3c1c62981451ec4b0b76ca8ee32c51
Tiziano Cappellari <tcappellari@nabertech.com>
2017-12-08 17:59:43 +0100

    test commented

--- dwaplatform/src/androidTest/java/com/dwaplatform/android/DWAplatformTest.kt
+++ dwaplatform/src/androidTest/java/com/dwaplatform/android/DWAplatformTest.kt
@@ -1,11 +1,13 @@
 package com.dwaplatform.android
 
+import android.content.Context
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
 import com.dwaplatform.android.payin.models.PayInConfiguration
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito
 
 @RunWith(AndroidJUnit4::class)
 class DWAplatformTest {
@@ -76,5 +78,32 @@ class DWAplatformTest {
 
 
     }
+
+    /*
+    @Test
+    fun test_balanceHelper() {
+        val context = InstrumentationRegistry.getTargetContext()
+
+        val hostName = "https://api.sandbox.dwaplatform.com"
+        val token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJleHAiOjE1MTI3NDU0MDAsImlhdCI6MTUxMjY1OTAwMCwiY2xpZW50SWQiOiJkYmI3YzEwYS03MzY3LTExZTctODgxMy1iZmUyY2ExMGJiOWEifQ.A1z1-uMuMdq_CrbNjT7xhzrUdJQ1cIIjoLFUqTIi63JUFi0fmkE0CxW-iVemo2VR8qyJjXW6CljJ-8p3cy0V5A"
+
+        val userId = "60e279ec-918b-11e7-8d55-dbaaa24cc5c2"
+        val accountId = "722c4d74-ba56-11e7-8487-a7b42609a5c1"
+
+        val balanceBuilder = DWAplatform.buildBalance()
+        val balanceHelperComponent = balanceBuilder.createBalanceHelperComponent(hostName, token, context)
+
+        balanceHelperComponent.balanceHelper.getAndUpdateCachedBalance(userId, accountId) { optMoney, optError ->
+
+            //
+            Assert.assertNull(optError)
+            Assert.assertNotNull(optMoney)
+
+
+
+        }
+
+        Thread.sleep(10000)
+    }*/
 
 }
