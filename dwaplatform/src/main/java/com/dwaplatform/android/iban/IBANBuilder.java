f1f9ef9796644e84f09f0406a7acd805b61fbb28
ingrid <fermingrid@gmail.com>
2018-01-08 10:28:12 +0100

    add iban api component

--- dwaplatform/src/main/java/com/dwaplatform/android/iban/IBANBuilder.java
+++ dwaplatform/src/main/java/com/dwaplatform/android/iban/IBANBuilder.java
@@ -1,12 +1,19 @@
 package com.dwaplatform.android.iban;
 
+import android.content.Context;
+
+import com.android.volley.toolbox.Volley;
+import com.dwaplatform.android.api.NetModule;
 import com.dwaplatform.android.iban.ui.IbanUIModule;
 import com.dwaplatform.android.models.DataAccount;
 
 
 public class IBANBuilder {
-
-
+    public IbanAPIComponent createIbanAPIComponent(Context context) {
+        return DaggerIbanAPIComponent.builder()
+                .netModule(new NetModule(Volley.newRequestQueue(context)))
+                .build();
+    }
 
     public IbanUIComponent createIBANUIComponent(String hostname, DataAccount configuration) {
         return DaggerIbanUIComponent.builder()
