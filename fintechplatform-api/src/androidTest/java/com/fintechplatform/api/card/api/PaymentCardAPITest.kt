af7cf60d9ce55eed7bd5e0f1e0787f49aacd521c
ingrid <fermingrid@gmail.com>
2018-08-16 10:45:34 +0100

    align to refactored names

--- fintechplatform-api/src/androidTest/java/com/fintechplatform/api/card/api/PaymentCardAPITest.kt
+++ fintechplatform-api/src/androidTest/java/com/fintechplatform/api/card/api/PaymentCardAPITest.kt
@@ -3,11 +3,14 @@ package com.fintechplatform.api.card.api
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
 import com.fintechplatform.api.FintechPlatformAPI
-import com.fintechplatform.api.card.models.PaymentCardItem
-import com.fintechplatform.api.cashin.models.CashInReply
+import com.fintechplatform.api.account.models.Account
+import com.fintechplatform.api.card.models.PaymentCard
+import com.fintechplatform.api.cashin.models.CashInResponse
+import com.fintechplatform.api.money.Currency
 import com.fintechplatform.api.money.Money
 import com.fintechplatform.api.net.ErrorCode
 import com.fintechplatform.api.net.NetHelper
+import com.fintechplatform.api.user.models.User
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
@@ -21,23 +24,30 @@ import java.util.concurrent.TimeUnit
 class PaymentCardAPITest {
     lateinit var hostName: String
     lateinit var accessToken: String
-    lateinit var tenantId: String
-    lateinit var userId: String
-    lateinit var accountId: String
+
+    lateinit var account: Account
 
     @Before
     fun setUp() {
-        /*hostName = ProcessInfo.processInfo.environment["HOSTNAME"]!
-        accessToken = ProcessInfo.processInfo.environment["ACCOUNT_TOKEN"]!
-        tenantId = ProcessInfo.processInfo.environment["TENANT_ID"]!
-        userId = ProcessInfo.processInfo.environment["OWNER_ID"]!
-        accountId = ProcessInfo.processInfo.environment["ACCOUNT_ID"]!*/
 
-        hostName = ""
-        accessToken = ""
-        tenantId = ""
-        userId = ""
-        accountId = ""
+        hostName = "http://www.sandbox.lightbankingservices.com/api/v1"
+
+        /**
+         * go to api-test path:
+         * fintech-platform-api-test user$
+         *
+         * and run this script to obtain ids and params:
+         *
+         * SDK_IOS_WORKSPACE_PATH="" ./node_modules/mocha/bin/mocha tests/mobile_sdk_test.js
+         */
+
+        val tenantId = "b0456cc4-5574-483e-b4f9-e886cc3fedfe"
+        val userId = "b3483f59-c7f2-41aa-9d45-e21dbf4722ca"
+        val accountId = "f3f931ca-9104-4a51-b74c-11370f358636"
+
+        accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJleHAiOjE1MzQ0OTcwNTgsImlhdCI6MTUzNDQxMDY1OCwidGVuYW50SWQiOiJiMDQ1NmNjNC01NTc0LTQ4M2UtYjRmOS1lODg2Y2MzZmVkZmUiLCJhY2NvdW50VHlwZSI6IlBFUlNPTkFMIiwib3duZXJJZCI6ImIzNDgzZjU5LWM3ZjItNDFhYS05ZDQ1LWUyMWRiZjQ3MjJjYSIsImFjY291bnRJZCI6ImYzZjkzMWNhLTkxMDQtNGE1MS1iNzRjLTExMzcwZjM1ODYzNiIsImp3dFR5cGUiOiJBQ0NPVU5UIiwic2NvcGUiOlsiTElOS0VEX0NBUkQiLCJMSU5LRURfQ0FSRF9DQVNIX0lOIl19.f6FWhNHp1ZNAtyRTtGypuGZEZPKLN0BnOHL2w_dmEbV1tORH9yo_OwJZ015LpfzHXyK_rnqfIl0gneFbFME0pg"
+
+        account = Account.personalAccount(User(UUID.fromString(tenantId), UUID.fromString(userId)), UUID.fromString(accountId))
 
     }
 
@@ -50,9 +60,9 @@ class PaymentCardAPITest {
         // get initial payment card for account user
         // getPaymentCard NOT_ACTIVE, expect two Cards
         val expectationGetCardsN = CountDownLatch(1)
-        var card: PaymentCardItem? = null
+        var card: PaymentCard? = null
         var cardsListOptError: Exception? = null
-        paymentCardAPI.registerCard(accessToken, userId, accountId, "PERSONAL", tenantId,"1234123412341234", "0122", "123", "EUR") {
+        paymentCardAPI.registerCard(accessToken, account,"1234123412341234", "0122", "123", Currency.EUR) {
             optCard, optError ->
             cardsListOptError = optError
             card = optCard
@@ -66,12 +76,12 @@ class PaymentCardAPITest {
         Assert.assertNotNull(card)
 
         // cashIn with invalid card
-        var cashIn1 : CashInReply? = null
+        var cashIn1 : CashInResponse? = null
         var cashInError1 : Exception? = null
         val expectationCashIn1 = CountDownLatch(1)
         val idempotency10eur = UUID.randomUUID().toString()
 
-        cashInAPI.cashIn(accessToken, userId, accountId, "PERSONAL", tenantId, card?.id!!, Money(33300000L, "EUR"), idempotency10eur) { optCashIn, optError ->
+        cashInAPI.cashIn(accessToken, account, card!!.cardId!!, Money(33300000L, "EUR"), idempotency10eur) { optCashIn, optError ->
             cashIn1 = optCashIn
             cashInError1 = optError
             expectationCashIn1.countDown()
