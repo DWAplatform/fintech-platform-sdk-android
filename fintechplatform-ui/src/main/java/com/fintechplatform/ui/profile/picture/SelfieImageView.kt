d01eb898c8a717c7d1dc56e64614c90542b0d850
ingrid <fermingrid@gmail.com>
2019-09-19 12:47:28 +0100

    add selfie imageview

---
+++ fintechplatform-ui/src/main/java/com/fintechplatform/ui/profile/picture/SelfieImageView.kt
@@ -0,0 +1,10 @@
+package com.fintechplatform.ui.profile.picture
+
+import android.content.Context
+import android.widget.ImageView
+
+
+class SelfieImageView(private val mcontext: Context): ImageView(mcontext) {
+
+
+}
